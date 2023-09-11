12db8da02141b3e399d23ae5bbdc4a7e54694a76
runphp <runphp@qq.com>
2023-09-11 18:08:10 +0800

    rm: gen-fonts.rb

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: process.env.NODE_ENV,
     entry: {
         common: ['./js/common.js'],
-        gitalk: ['./js/gitalk.js']
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, '_site/dist'),
-    }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "node_modules/gitalk/dist" },
+                { from: "node_modules/bootstrap-icons/font/fonts", to: "fonts"}
+            ],
+        }),
+    ],
 };
