7ba25cbea4a593678caab555e18ece0ca518b4fd
runphp <runphp@qq.com>
2023-09-13 16:10:33 +0800

    add content.html

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
 import './color-modes'
 import Dropdown from 'bootstrap/js/src/dropdown'
 import Offcanvas from 'bootstrap/js/src/offcanvas'
+import ScrollSpy from 'bootstrap/js/src/scrollspy'
 
 export default {
     Dropdown,
     Offcanvas,
+    ScrollSpy
 }
